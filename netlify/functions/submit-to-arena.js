b4073110946a1a5f5d80f9698f85854fb986455f
BreeD2 <dormanb@vcu.edu>
2024-03-22 14:49:20 -0400

    cha

--- netlify/functions/submit-to-arena.js
+++ netlify/functions/submit-to-arena.js
@@ -5,12 +5,15 @@ exports.handler = async (event) => {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    const { imageUrl, imageTitle, imageDescription } = JSON.parse(event.body);
+    const { imageTitle, imageUrl, imageDescription } = JSON.parse(event.body);
     const token = process.env.ARENA_ACCESS_TOKEN;
 
     if (!token) {
         console.error('ARENA_ACCESS_TOKEN is not defined.');
-        return { statusCode: 500, body: JSON.stringify({ message: "Internal Server Error: Missing Are.na access token." }) };
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Missing Are.na access token." })
+        };
     }
 
     try {
@@ -19,17 +22,27 @@ exports.handler = async (event) => {
             'Content-Type': 'application/json'
         };
 
-        // Formatting the title and description in Markdown to include in the :content parameter
-        const markdownContent = `# ${imageTitle}\n\n${imageDescription}`;
+        // Formatting the description to include both title and description.
+        let fullDescription = `**${imageTitle}**\n${imageDescription}`;
 
-        const response = await axios.post(`https://api.are.na/v2/channels/${process.env.ARENA_CHANNEL_SLUG}/blocks`, {
-            source: imageUrl, // for images, links, or embeds
-            content: markdownContent // for the combined title and description
+        const response = await axios.post(`https://api.are.na/v2/channels/testing-wrkgn_q6vbg/blocks`, {
+            source: imageUrl, // Correctly using the source parameter for the image URL.
+            description: fullDescription, // The combined title and description.
         }, { headers });
 
-        return { statusCode: 200, body: JSON.stringify({ message: "Image submitted successfully to Are.na." }) };
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Image submitted successfully." })
+        };
     } catch (error) {
         console.error('Error submitting to Are.na:', error);
-        return { statusCode: error.response.status, body: JSON.stringify({ message: "Failed to submit image to Are.na.", error: error.message, detail: error.response?.data }) };
+        return {
+            statusCode: error.response?.status || 500,
+            body: JSON.stringify({
+                message: "Failed to submit image.",
+                error: error.message,
+                detail: error.response?.data,
+            })
+        };
     }
 };

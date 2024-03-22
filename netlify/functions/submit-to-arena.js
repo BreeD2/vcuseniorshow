f7ade4e45d0b43e5edf9fbe11ba06bb8c0bdde75
BreeD2 <dormanb@vcu.edu>
2024-03-22 13:54:35 -0400

    hub

--- netlify/functions/submit-to-arena.js
+++ netlify/functions/submit-to-arena.js
@@ -5,41 +5,36 @@ exports.handler = async (event) => {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    // Parse the incoming event body to access title, imageUrl, and description
     const { imageTitle, imageUrl, imageDescription } = JSON.parse(event.body);
-
     const token = process.env.ARENA_ACCESS_TOKEN;
+
     if (!token) {
         console.error('ARENA_ACCESS_TOKEN is not defined.');
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Internal Server Error: Missing Are.na access token." })
+            body: JSON.stringify({ message: "Missing Are.na access token." })
         };
     }
 
     try {
-        // Your axios request using the token
         const headers = {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
         };
 
-        // Constructing the description to include both title and description if available
-        let fullDescription = imageTitle ? `${imageTitle}\n\n${imageDescription}` : imageDescription;
+        // Including the title within the description field
+        let combinedDescription = imageTitle ? `${imageTitle}\n\n${imageDescription}` : imageDescription;
 
-        // Submit the POST request to Are.na
         const response = await axios.post(`https://api.are.na/v2/channels/testing-wrkgn_q6vbg/blocks`, {
             content: imageUrl,
-            description: fullDescription // Combined title and description
+            description: combinedDescription
         }, { headers });
 
-        // Successful response handling
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Image submitted successfully to Are.na." })
         };
     } catch (error) {
-        // Error handling
         console.error('Error submitting to Are.na:', error);
         return {
             statusCode: error.response.status,

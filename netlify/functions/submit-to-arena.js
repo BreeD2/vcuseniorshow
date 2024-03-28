b59c9537ef4ca6a14629d975e472cce626037fca
BreeD2 <dormanb@vcu.edu>
2024-03-27 20:00:19 -0400

    hey

--- netlify/functions/submit-to-arena.js
+++ netlify/functions/submit-to-arena.js
@@ -5,13 +5,9 @@ exports.handler = async (event) => {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    // Parse the incoming event body to access imageUrl
-    const { imageUrl } = JSON.parse(event.body);
-
-    // Access the ARENA_ACCESS_TOKEN environment variable
+    const { imageUrl, imageTitle, imageDescription } = JSON.parse(event.body);
     const token = process.env.ARENA_ACCESS_TOKEN;
 
-    // Check if the token is available
     if (!token) {
         console.error('ARENA_ACCESS_TOKEN is not defined.');
         return {
@@ -21,32 +17,28 @@ exports.handler = async (event) => {
     }
 
     try {
-        // Your axios request using the token
         const headers = {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
         };
 
-        // Example POST request to Are.na
         const response = await axios.post(`https://api.are.na/v2/channels/vcu-senior-show-2024/blocks`, {
-            source: imageUrl, // Use the parsed imageUrl
-            description: "User submitted image via Netlify function"
+            source: imageUrl,
+            description: `${imageTitle}\n\n${imageDescription}` // Combining title and description
         }, { headers });
 
-        // Successful response handling
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Image submitted successfully to Are.na." })
         };
     } catch (error) {
-        // Error handling
         console.error('Error submitting to Are.na:', error);
         return {
-            statusCode: error.response.status,
+            statusCode: error.response?.status || 500,
             body: JSON.stringify({
                 message: "Failed to submit image to Are.na.",
                 error: error.message,
-                detail: error.response.data
+                detail: error.response?.data
             })
         };
     }

68b92d75a01a9bad02e17aaa542064403785c3f7
BreeD2 <dormanb@vcu.edu>
2024-03-29 10:37:35 -0400

    submit

--- netlify/functions/submit-to-arena.js
+++ netlify/functions/submit-to-arena.js
@@ -2,16 +2,21 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
     if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: JSON.stringify({ message: 'Method Not Allowed' }) };
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: 'Method Not Allowed' })
+        };
     }
 
-    // Destructure yourTitle from the parsed JSON body along with other fields
-    const { yourName, title, yourDescription, yourLink } = JSON.parse(event.body);
-
+    const { yourName, title, yourDescription, yourLink, relatedChannelId } = JSON.parse(event.body);
     const token = process.env.ARENA_ACCESS_TOKEN;
+
     if (!token) {
         console.error('ARENA_ACCESS_TOKEN is not defined.');
-        return { statusCode: 500, body: JSON.stringify({ message: 'Internal Server Error: Missing Are.na access token.' }) };
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error: Missing Are.na access token.' })
+        };
     }
 
     try {
@@ -20,15 +25,17 @@ exports.handler = async (event) => {
             'Content-Type': 'application/json'
         };
 
-        // Formatting the description to include name, title, and description on separate lines using markdown for better readability.
-        const formattedDescription = `**Name:** ${yourName}\n\n**Title:** ${title}\n\n**Description:** ${yourDescription}`;
+        const formattedDescription = `**Name:** ${yourName}\n\n**Title:** ${title}\n\n**Description:** ${yourDescription}\n\n**Related Channel ID:** ${relatedChannelId}`;
 
         const response = await axios.post(`https://api.are.na/v2/channels/vcu-senior-show-2024/blocks`, {
             source: yourLink,
-            description: formattedDescription // Using the formatted description that now includes the title
+            description: formattedDescription
         }, { headers });
 
-        return { statusCode: 200, body: JSON.stringify({ message: 'Submission successful.', response: response.data }) };
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Submission successful.', response: response.data })
+        };
     } catch (error) {
         console.error('Error submitting to Are.na:', error);
         return {
